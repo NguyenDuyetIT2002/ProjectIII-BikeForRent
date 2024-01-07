08e86b7f155eaa6a985a7bf06b6a1d77bee7a747
bachthutruong <vuduybachvp@gmail.com>
2024-01-07 23:19:27 +0700

    admin dashboard

---
+++ bfr-code/frontend/src/pages/admin/banbike/banbike.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BanBike() {
+
+  return (
+    <div className="bg-white-100 min-h-screen">
+      <p>BanBike</p>
+    </div>
+  );
+}
+
+export default BanBike;
