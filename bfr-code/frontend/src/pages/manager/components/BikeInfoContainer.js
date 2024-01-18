6de855193ac83e7bb806d99a4d8a596c6ef1e671
AnUnluckyGuy <daz2002pro@gmail.com>
2024-01-18 10:29:58 +0700

    upd manager

--- bfr-code/frontend/src/pages/manager/components/BikeInfoContainer.js
+++ bfr-code/frontend/src/pages/manager/components/BikeInfoContainer.js
@@ -29,68 +29,65 @@ const BikeInfoContainer = ({bikeInfo, deleteBike}) => {
     };
 
     return (
-        <div className="flex flex-row my-10 p-10 border-2 border-black rounded-lg">
-            <img src={bikeInfo.image} alt='Bike image'></img>   
-            <table className='table ml-10'>
-                <tbody>
-                    <tr>
-                        <td>{bikeInfo.name}</td>
-                    </tr>
-                    <tr>
-                        <td>Type: {bikeInfo.type}</td>
+        <div className='flex flex-col my-10 border border-black rounded-lg'>
+            <div className="flex flex-row m-10">
+                <img src={bikeInfo.image} alt='Bike image'></img>   
+                <table className='table ml-10'>
+                    <tbody>
+                        <tr>
+                            <td>{bikeInfo.name}</td>
+                        </tr>
+                        <tr>
+                            <td>Type: {bikeInfo.type}</td>
 
-                    </tr>
-                    <tr>
-                        <td>Status: {bikeInfo.status}</td>
-                    </tr>
-                    <tr>
-                        <td>Rent price: {bikeInfo.price}.000 VND</td>
-                    </tr>
-                    <tr>
-                        <td>Description: {bikeInfo.description}</td>
-                    </tr>
-                    <tr>
-                        <td>Rented by: </td>
-                    </tr>
-                    <tr>
-                        <td>Ban Report: {bikeInfo.banRequestAmount}</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <button className='px-2 py-1 border-2 border-black rounded-lg' onClick={handleClickOpen}>
-                                Delete
-                            </button>
-                        </td>
-
-                        <td>
-                            <button className='px-2 py-1 border-2 border-black rounded-lg' onClick={handleEditClick}>
-                                Edit
-                            </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {"Xác nhận xóa xe"}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Bạn muốn thực hiện xóa xe {bikeInfo.name} khỏi cửa hàng của bạn?
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleDeleteClick}>Xác nhận</Button>
-                    <Button onClick={handleClose} autoFocus>
-                        Hủy
-                    </Button>
-                </DialogActions>
-            </Dialog>
+                        </tr>
+                        <tr>
+                            <td>Status: {bikeInfo.status}</td>
+                        </tr>
+                        <tr>
+                            <td>Rent price: {bikeInfo.price}.000 VND</td>
+                        </tr>
+                        <tr>
+                            <td>Description: {bikeInfo.description}</td>
+                        </tr>
+                        <tr>
+                            <td>Rented by: </td>
+                        </tr>
+                        <tr>
+                            <td>Ban Report: {bikeInfo.banRequestAmount}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        {"Xác nhận xóa xe"}
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Bạn muốn thực hiện xóa xe {bikeInfo.name} khỏi cửa hàng của bạn?
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleDeleteClick}>Xác nhận</Button>
+                        <Button onClick={handleClose} autoFocus>
+                            Hủy
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+            <div className='flex flex-row'>
+                <button className='w-full h-10 bg-red-300 hover:bg-red-500 rounded-bl-lg text-lg font-medium' onClick={handleClickOpen}>
+                    Delete
+                </button>
+                <button className='w-full h-10 bg-yellow-300 hover:bg-yellow-500 rounded-br-lg text-lg font-medium' onClick={handleEditClick}>
+                    Edit
+                </button>
+            </div>
         </div>
     )
 }
---
+++ bfr-code/frontend/src/pages/manager/components/ExpiredOrderContainer.js
@@ -0,0 +1,98 @@
+import React from 'react';
+
+const ExpiredOrderContainer = ({orderInfo, handelOnClick}) => {
+    function getDateTime(timeStr) {
+        const dateTime = timeStr.split('T');
+        const date = dateTime[0].split('-');
+        const time = dateTime[1].split('.')[0].split(':');
+        var hour = parseInt(time[0], 10) + 7;
+        var minute = parseInt(time[1], 10);
+        var second = parseInt(time[2], 10);
+        var day = parseInt(date[2], 10);
+        var month = parseInt(date[1], 10);
+        var year = parseInt(date[0], 10); 
+        if (hour >= 24) {
+            hour -= 24;
+            day += 1;
+
+            if (day == 29){
+                if (month == 2) {
+                    if (year % 4 == 0){
+                        if (year % 100 == 0 && year % 400 != 0){
+                            day = 1;
+                            month = 3;
+                        }
+                    }
+                    else {
+                        day = 1;
+                        month = 3;
+                    }
+                }
+            } 
+            else if (day == 30){
+                if (month == 2) {
+                    day = 1;
+                    month = 3;
+                }
+            }
+            else if (day == 31){
+                if (month == 4 || month == 6 || month == 9 || month == 11){
+                    day = 1;
+                    month += 1;
+                }
+            }
+            else if (day == 32){
+                day = 1;
+                month += 1;
+                if (month > 12) {
+                    month = 1
+                    year += 1;
+                }
+            }
+
+        }
+
+        if (hour >= 10)hour = hour.toString();
+        else hour = '0' + hour.toString();
+
+        if (day >= 10)day = day.toString();
+        else day = '0' + day.toString();
+
+        if (month >= 10)month = month.toString();
+        else month = '0' + month.toString();
+
+        return `${hour}:${minute}:${second} ${day}-${month}-${year}`;
+    }
+
+    return (
+        <div className="flex flex-col my-10 border border-black rounded-lg">
+            <table className='table ml-10 m-5'>
+                <tbody>
+                    <tr>
+                        <td>Người thuê: {orderInfo.customer_name}</td>
+                    </tr>
+                    <tr>
+                        <td>Xe thuê: {orderInfo.bike_id}</td>
+                    </tr>
+                    <tr>
+                        <td>Bắt đầu thuê: {getDateTime(orderInfo.startTime)}</td>
+                    </tr>
+                    <tr>
+                        <td>Kết thúc thuê: {getDateTime(orderInfo.endTime)}</td>
+                    </tr>
+                    <tr>
+                        <td>Chi phí (VND): {orderInfo.price}.000</td>
+                    </tr>
+                    <tr>
+                        <td>Trạng thái: {orderInfo.status}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <button className='h-10 bg-red-300 hover:bg-red-500 rounded-b-lg text-lg font-medium' onClick={() => handelOnClick(orderInfo.customer_id)}>
+                Report
+            </button>
+        </div>
+    );
+}
+
+export default ExpiredOrderContainer;
